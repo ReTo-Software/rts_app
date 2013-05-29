0a64cc3803e30de74b840b4cca64295c1d092aef
Tony Briceño <tonybp18@gmail.com>
2013-05-29 14:47:55 -0430

    segunda modificacion
    para el trolo de fran
    + pol sapo

--- index2.php
+++ index2.php
@@ -2,6 +2,7 @@
 <?php if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) ob_start("ob_gzhandler"); else ob_start(); 
 
 # ESTO ES UNA PRUEBA PARA EL GIT
+#segunda modificacion
 
 ?>
 <!doctype html>
